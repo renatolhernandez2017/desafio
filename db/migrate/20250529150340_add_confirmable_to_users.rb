0ea52a3a1dd53f919b677b41e2534247cf6d56f5
Renato Hernandez <renatolhernandez@hotmail.com>
2025-05-29 15:30:14 +0000

    Simular o envio de email em desenvolvimento

--- db/migrate/20250529150340_add_confirmable_to_users.rb
+++ db/migrate/20250529150340_add_confirmable_to_users.rb
@@ -3,5 +3,6 @@ class AddConfirmableToUsers < ActiveRecord::Migration[7.1]
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
+    add_index :users, :confirmation_token, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_29_151405) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
