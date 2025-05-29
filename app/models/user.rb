b7e213b5c6ca87e357e2b6c695b104210d60c57c
Renato Hernandez <renatolhernandez@hotmail.com>
2025-05-29 15:07:48 +0000

    Configurar o devise

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+
+  validates :name, presence: true, length: { in: 3..255 }
+  validates :username, presence: true, uniqueness: true, length: { in: 5..50 }, format: { with: /\A[a-zA-Z0-9\-_]+\z/ }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { in: 20..100 }, confirmation: true
+end
